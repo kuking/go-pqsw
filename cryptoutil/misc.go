25e94236a274c34310dc88c6c156dcdae8e48ef5
Eduardo ES Riccardi <ed@kukino.uk>
2020-06-22 20:01:22 +0100

    SimpleSuperTriple AEAD

--- cryptoutil/misc.go
+++ cryptoutil/misc.go
@@ -1,9 +1,13 @@
 package cryptoutil
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"errors"
+	"golang.org/x/crypto/scrypt"
 )
 
 type KeyType uint8
@@ -62,6 +66,18 @@ var CipherTextSizeByKeyType = map[KeyType]KemSizes{
 	KeyTypeKyber1024:      {3168, 1568, 1504, 32},
 }
 
+type SCryptParameters struct {
+	N int
+	R int
+	P int
+}
+
+var CurrentSCryptParameters = SCryptParameters{
+	N: 65536 * 2,
+	R: 8,
+	P: 1,
+}
+
 func QuickSha256(b []byte) []byte {
 	h := sha256.New()
 	h.Write(b)
@@ -87,4 +103,81 @@ func ConcatAll(parts ...[]byte) (res []byte) {
 		res = append(res, part...)
 	}
 	return res
+}
+
+func AES256CGMSeal(key, nonce, plainText []byte) (cipherText []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return
+	}
+	cipherText = aead.Seal(nil, nonce, plainText, nil)
+	return
+}
+
+func AES256CGMOpen(key, nonce, cipherText []byte) (plainText []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return
+	}
+	return aead.Open(nil, nonce, cipherText, nil)
+}
+
+// Creates a []byte encrypted with Triple AES256-CGM using Scrypt to derive the password.
+func SimpleSuperTripleEncrypt(plainText []byte, password string) (cipherText []byte, err error) {
+	keyLen := 32 * 3
+	salt := RandBytes(keyLen)
+	nonces := RandBytes(12 * 3)
+	key, err := scrypt.Key([]byte(password), salt, CurrentSCryptParameters.N, CurrentSCryptParameters.R, CurrentSCryptParameters.P, keyLen)
+	if err != nil {
+		return
+	}
+	cipher1, err := AES256CGMSeal(key[0:32], nonces[0:12], plainText)
+	if err != nil {
+		return
+	}
+	cipher2, err := AES256CGMSeal(key[32:64], nonces[12:24], cipher1)
+	if err != nil {
+		return
+	}
+	cipher3, err := AES256CGMSeal(key[64:96], nonces[24:36], cipher2)
+	if err != nil {
+		return
+	}
+	cipherText = make([]byte, len(cipher3)+len(salt)+len(nonces)+2)
+	cipherText[0] = 0xbe
+	cipherText[1] = 0xef
+	copy(cipherText[2:], salt)
+	copy(cipherText[2+len(salt):], nonces)
+	copy(cipherText[2+len(salt)+len(nonces):], cipher3)
+	return
+}
+
+func SimpleSuperTripleDecrypt(cipherText []byte, password string) (plainText []byte, err error) {
+	if cipherText[0] != 0xbe || cipherText[1] != 0xef {
+		err = errors.New("magic bytes are not valid ... might not be encrypted with this method")
+		return
+	}
+	keyLen := 32 * 3
+	salt := make([]byte, keyLen)
+	copy(salt, cipherText[2:])
+	nonces := make([]byte, 12*3)
+	copy(nonces, cipherText[2+len(salt):])
+	key, err := scrypt.Key([]byte(password), salt, CurrentSCryptParameters.N, CurrentSCryptParameters.R, CurrentSCryptParameters.P, keyLen)
+	cipher2, err := AES256CGMOpen(key[64:96], nonces[24:36], cipherText[2+len(salt)+len(nonces):])
+	if err != nil {
+		return
+	}
+	cipher1, err := AES256CGMOpen(key[32:64], nonces[12:24], cipher2)
+	if err != nil {
+		return
+	}
+	return AES256CGMOpen(key[0:32], nonces[0:12], cipher1)
 }
--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/kuking/go-frodokem v1.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 )
