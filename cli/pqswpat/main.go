cbd10ef13cadca539ff04c0cedb1a527c0fdee4e
Eduardo ES Riccardi <ed@kukino.uk>
2020-07-02 22:51:11 +0100

    pqswpat work in progress

--- cli/pqswpat/main.go
+++ cli/pqswpat/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"github.com/kuking/go-pqsw/config"
+	"github.com/kuking/go-pqsw/wire"
+	"io"
+	"net"
 	"os"
 	"regexp"
 )
@@ -15,7 +20,7 @@ const (
 var configFile string
 var workMode int
 var doHelp bool
-var listenAddr string
+var hostPort string
 
 func doParsing() bool {
 	flag.StringVar(&configFile, "c", "pqswpat.conf", "PQSW config file")
@@ -53,13 +58,73 @@ func doParsing() bool {
 		fmt.Println("Please provide a forwarding in the format: host:port")
 		return false
 	}
-	listenAddr = maps[1] + ":" + maps[2]
+	hostPort = maps[1] + ":" + maps[2]
 	return true
+}
+
+func handleServerConnection(conn net.Conn, cfg *config.Config) {
+	var err error
+	sw, err := wire.ServerHandshake(conn, cfg)
+	if err != nil {
+		fmt.Println(fmt.Errorf("server could not establish sercure wire, err=%v", err))
+		return
+	}
+	go lineCopier("server' stdin->wire", os.Stdin, sw)
+	go lineCopier("server' wire->stdout", sw, os.Stdout)
+}
+
+func doListen(cfg *config.Config) {
+	ln, err := net.Listen("tcp", hostPort)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Listening %v ...\n", ln.Addr())
+	for {
+		conn, err := ln.Accept()
+		fmt.Printf("New connection from %v ...\n", conn.RemoteAddr())
+		if err != nil {
+			fmt.Println("Error accepting connection", err)
+		} else {
+			go handleServerConnection(conn, cfg)
+		}
+	}
+}
+
+func doConnect(cfg *config.Config) {
+	fmt.Printf("Connecting to %v ...\n", hostPort)
+	ln, err := net.Dial("tcp", hostPort)
+	if err != nil {
+		panic(err)
+	}
+	sw, err := wire.ClientHandshake(ln, cfg)
+	if err != nil {
+		panic(err)
+	}
+	go lineCopier("client' stdin->wire", os.Stdin, sw)
+	lineCopier("client' wire->stdout", sw, os.Stdout)
+}
+
+func lineCopier(desc string, r io.ReadCloser, w io.WriteCloser) {
+	defer r.Close()
+	defer w.Close()
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		w.Write(append(scanner.Bytes(), '\n'))
+	}
+	fmt.Println("Closing", desc)
 }
 
 func main() {
 	if doParsing() {
-		fmt.Println("do work")
-
+		cfg, err := config.LoadFromInteractive(configFile)
+		if err != nil {
+			fmt.Println(fmt.Errorf("could not read the config file, err=%v", err))
+			os.Exit(-1)
+		}
+		if workMode == WorkModeServer {
+			doListen(cfg)
+		} else {
+			doConnect(cfg)
+		}
 	}
 }
