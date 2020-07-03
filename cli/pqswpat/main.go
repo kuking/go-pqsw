af59ee26c089ec0d062fcf63d572d5eb3f6f7005
Eduardo ES Riccardi <ed@kukino.uk>
2020-07-04 00:14:29 +0100

    pqswpat work-in-progress

--- cli/pqswpat/main.go
+++ cli/pqswpat/main.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"sync"
 )
 
 const (
@@ -27,8 +28,15 @@ func doParsing() bool {
 	flag.BoolVar(&doHelp, "h", false, "Show usage")
 	flag.Parse()
 	if doHelp {
-		fmt.Printf("Usage of %v: <server|client> <host:port>\n", os.Args[0])
-		fmt.Println("A simple broadcast server/client. Server receives connections and forwards messages to all connected clients.")
+		fmt.Printf(`Usage of %v: <server|client> <host:port>
+
+A simple broadcast server and client. Server accepts connections and forwards messages to all connected clients.
+Protocol is text based, messages are delimited by an end-of-line characters. Connection is over a PQSW secure wire.
+When working as server it will output the traffic to its stdout but content sent to stdin will be ignored.
+As client, broadcasted messages coming from the server will be output to stdout and anything sent to stdin will be
+further forwarded to the server and be broadcasted to all clients (including itself, echoing it.)
+
+`, os.Args[0])
 		flag.PrintDefaults()
 		fmt.Println()
 		return false
@@ -62,6 +70,16 @@ func doParsing() bool {
 	return true
 }
 
+type ClientConnection struct {
+	Name           string
+	Wire           *wire.SecureWire
+	ServerToClient chan string
+}
+
+var ClientConnectionsLock sync.RWMutex
+var ClientConnections = make([]ClientConnection, 0)
+var ClientToServer = make(chan string, 100)
+
 func handleServerConnection(conn net.Conn, cfg *config.Config) {
 	var err error
 	sw, err := wire.ServerHandshake(conn, cfg)
@@ -69,15 +87,61 @@ func handleServerConnection(conn net.Conn, cfg *config.Config) {
 		fmt.Println(fmt.Errorf("server could not establish sercure wire, err=%v", err))
 		return
 	}
-	go lineCopier("server' stdin->wire", os.Stdin, sw)
-	go lineCopier("server' wire->stdout", sw, os.Stdout)
+	conn.RemoteAddr().String()
+	clientConnection := ClientConnection{
+		Name:           conn.RemoteAddr().String(),
+		Wire:           sw,
+		ServerToClient: make(chan string, 10),
+	}
+	ClientConnectionsLock.Lock()
+	ClientConnections = append(ClientConnections, clientConnection)
+	ClientConnectionsLock.Unlock()
+
+	go handleClientInServer(clientConnection)
 }
 
-func doListen(cfg *config.Config) {
+func handleClientInServer(conn ClientConnection) {
+	wireChan := make(chan string)
+	scanner := bufio.NewScanner(conn.Wire)
+	go scannerToChan(scanner, wireChan)
+	for {
+		select {
+		case msg := <-conn.ServerToClient:
+			conn.Wire.Write(append([]byte(msg), '\n'))
+		case msg := <-wireChan:
+			fmt.Println(conn.Name, "->", msg)
+			ClientToServer <- msg
+		}
+		fmt.Println("handleClientInServer loop")
+	}
+}
+
+func scannerToChan(scanner *bufio.Scanner, wireChan chan string) {
+	for scanner.Scan() {
+		wireChan <- scanner.Text()
+	}
+	close(wireChan)
+}
+
+func serverBroadcaster() {
+	for {
+		select {
+		case msg := <-ClientToServer:
+			ClientConnectionsLock.Lock()
+			for _, clientConn := range ClientConnections {
+				clientConn.ServerToClient <- msg
+			}
+			ClientConnectionsLock.Unlock()
+		}
+	}
+}
+
+func doServer(cfg *config.Config) {
 	ln, err := net.Listen("tcp", hostPort)
 	if err != nil {
 		panic(err)
 	}
+	go serverBroadcaster()
 	fmt.Printf("Listening %v ...\n", ln.Addr())
 	for {
 		conn, err := ln.Accept()
@@ -90,7 +154,7 @@ func doListen(cfg *config.Config) {
 	}
 }
 
-func doConnect(cfg *config.Config) {
+func doClient(cfg *config.Config) {
 	fmt.Printf("Connecting to %v ...\n", hostPort)
 	ln, err := net.Dial("tcp", hostPort)
 	if err != nil {
@@ -122,9 +186,9 @@ func main() {
 			os.Exit(-1)
 		}
 		if workMode == WorkModeServer {
-			doListen(cfg)
+			doServer(cfg)
 		} else {
-			doConnect(cfg)
+			doClient(cfg)
 		}
 	}
 }
